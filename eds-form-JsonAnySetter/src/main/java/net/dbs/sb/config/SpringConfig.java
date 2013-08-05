3ad34ba5668ac8cf0ff0c9f8d44055f46e2e3a70
dbs <dbs@diic36.com>
2013-08-05 13:50:36 +0800

    rmoved useless content

--- eds-form-JsonAnySetter/src/main/java/net/dbs/sb/config/SpringConfig.java
+++ eds-form-JsonAnySetter/src/main/java/net/dbs/sb/config/SpringConfig.java
@@ -1,20 +1,13 @@
 package net.dbs.sb.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.MediaType;
-import org.springframework.util.ClassUtils;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -22,7 +15,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class SpringConfig extends WebMvcConfigurerAdapter {
 
-    private static final boolean jsonAnySetterPresent = ClassUtils.isPresent("com.fasterxml.jackson.annotation.JsonAnySetter", WebMvcConfigurationSupport.class.getClassLoader());
 
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
@@ -38,12 +30,5 @@ public class SpringConfig extends WebMvcConfigurerAdapter {
 	public ClassPathResource randomdata() {
 		return new ClassPathResource("/randomdata.csv.compressed");
 	}
-
-	@Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        Map<String, MediaType> medias = new HashMap<String, MediaType>();
-        if (jsonAnySetterPresent)
-            medias.put("json", MediaType.valueOf("application/json"));
-        configurer.mediaTypes(medias);
-    }
+	
 }
