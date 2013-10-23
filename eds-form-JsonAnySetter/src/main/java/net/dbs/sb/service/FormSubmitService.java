79247c34fd4568287e5dac9bd199308bcd861fcb
dbs <dbs@diic36.com>
2013-10-23 22:33:04 +0800

    Workaround for validation loss when form submit

--- eds-form-JsonAnySetter/src/main/java/net/dbs/sb/service/FormSubmitService.java
+++ eds-form-JsonAnySetter/src/main/java/net/dbs/sb/service/FormSubmitService.java
@@ -6,15 +6,21 @@ import java.lang.invoke.MethodHandles;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +35,9 @@ import net.dbs.sb.FormBean;
 public class FormSubmitService {
 
     private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    
+    @Autowired 
+    protected org.springframework.validation.Validator validator;
     
 	@ExtDirectMethod(value=FORM_POST)
 	public ExtDirectFormPostResult handleFormMultipartSubmit(
@@ -63,19 +72,22 @@ public class FormSubmitService {
 	
 	@ExtDirectMethod(value=ExtDirectMethodType.FORM_POST_JSON)
     public ExtDirectFormPostResult handleFormSubmitNoMultipartFile(
-            //@RequestBody  MultiValueMap<String, String> body,
             @Valid FormBean bean) throws Exception {
+	    DataBinder binder = new DataBinder(bean);
+        binder.setValidator(this.validator);
+        validator.validate(bean, binder.getBindingResult());
+	    
         logger.debug("any [{}]", bean.any());
-        ExtDirectFormPostResult result = new ExtDirectFormPostResult();
+        ExtDirectFormPostResult formPostResult = new ExtDirectFormPostResult(binder.getBindingResult());
         String resultString = "Server received: \n" + bean.toString() + "\n";
         if(bean.any().isEmpty()){
             //throw new Exception("Error: @JsonAnySetter not filled");
             resultString = "Error: @JsonAnySetter not filled";
-            result.setSuccess(false);
+            formPostResult.setSuccess(false);
         }
 
-        result.addResultProperty("response", resultString);
-        return result;
+        formPostResult.addResultProperty("response", resultString);
+        return formPostResult;
     }
 	
 	@ExtDirectMethod(value=ExtDirectMethodType.FORM_POST_JSON)
